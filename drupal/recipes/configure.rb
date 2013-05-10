99396f1e4e3780e892e742bbfa1e3e27b0b77bde
Paul Nickerson <paul.nickerson@grooveshark.com>
2013-05-10 14:43:46 -0400

    cleanup

--- drupal/recipes/configure.rb
+++ drupal/recipes/configure.rb
@@ -1,19 +1,19 @@
 execute "mysql-grant-privileges" do
-  command "/usr/bin/mysql -h#{node['mysql']['hostname']} -u#{node['mysql']['master_username']} -p#{node['mysql']['master_password']} < /tmp/grant-privs.sql"
+  command "/usr/bin/mysql -h#{node['mysql']['hostname']} -u#{node['mysql']['master_username']} -p#{node['mysql']['master_password']} < /tmp/create-db-and-grant-privs.sql"
   action :nothing
 end
 
 
 template "Grant privileges to mysql account for app db" do
-  path "/tmp/grant-privs.sql"
-  source "grant-privs.sql.erb"
+  path "/tmp/create-db-and-grant-privs.sql"
+  source "create-db-and-grant-privs.sql.erb"
   owner "root"
   group "root"
   mode "0600"
   variables(
     :username => node['drupal']['database']['username'],
     :password => node['drupal']['database']['password'],
-    :database => node['drupal']['database']['database']
+    :database => node['drupal']['database']['database_name']
   )
-  notifies :run, "execute[mysql-grant-privileges]", :immediately
+  notifies :run, resources(:execute => "mysql-grant-privileges"), :immediately
 end
