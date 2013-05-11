39d373c1841df58c99b7b1aa43a85311dcc29fd2
Paul Nickerson <paul.nickerson@grooveshark.com>
2013-05-10 21:28:07 -0400

    clean up clean up everybody everywhere

---
+++ drupal/attributes/default.rb
@@ -0,0 +1,2 @@
+default[:general_settings][:drupal][:repo_url] = "http://git.drupal.org/project/drupal.git"
+default[:general_settings][:drupal][:repo_branch] = "8.x"
--- drupal/recipes/configure.rb
+++ drupal/recipes/configure.rb
@@ -1,19 +1,23 @@
-execute "mysql-grant-privileges" do
-  command "/usr/bin/mysql -h#{node['mysql']['hostname']} -u#{node['mysql']['master_username']} -p#{node['mysql']['master_password']} < /tmp/create-db-and-grant-privs.sql"
-  action :nothing
+execute "mysql-create-db-and-grant-privs" do
+    @cmd = ["/usr/bin/mysql",
+            "-h'#{node[:adhoc_settings][:mysql][:hostname]}'",
+            "-u'#{node[:adhoc_settings][:mysql][:master_username]}'",
+            "-p'#{node[:adhoc_settings][:mysql][:master_password]}'",
+            "< /tmp/create-db-and-grant-privs.sql"]
+    command @cmd.join(" ")
+    action :nothing
 end
 
-
 template "Grant privileges to mysql account for app db" do
-  path "/tmp/create-db-and-grant-privs.sql"
-  source "create-db-and-grant-privs.sql.erb"
-  owner "root"
-  group "root"
-  mode "0600"
-  variables(
-    :username => node['drupal']['database']['username'],
-    :password => node['drupal']['database']['password'],
-    :database => node['drupal']['database']['database_name']
-  )
-  notifies :run, resources(:execute => "mysql-grant-privileges"), :immediately
+    path "/tmp/create-db-and-grant-privs.sql"
+    source "create-db-and-grant-privs.sql.erb"
+    owner "root"
+    group "root"
+    mode "0600"
+    variables(
+        :username => node[:adhoc_settings][:drupal][:database][:username],
+        :password => node[:adhoc_settings][:drupal][:database][:password],
+        :database => node[:adhoc_settings][:drupal][:database][:database_name]
+    )
+    notifies :run, resources(:execute => "mysql-create-db-and-grant-privs"), :immediately
 end
